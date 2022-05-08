ba10fc320bfda4c83349c69fbd03bdd2e1fea042
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:56:21 +0430

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // In do khat code adede random beyne 0 ta 100 tolid mi konad.
+            //Random rand = new Random();
+            //int adad = rand.Next(0,100);
+
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Console.WriteLine("Welcome");
+                Console.ReadKey();
+                goto N;
+            } else 
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Go out");
+            }
+        N:
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Enter your score:");
+            int score = int.Parse(Console.ReadLine());
+            if (score > 20)
+            {
+                Console.WriteLine("The score is from 20");
+                Console.ReadKey();
+                Console.Clear();
+                goto N;
+            }
+            else
+            {
+                int deducted = 20 - score;
+                Console.WriteLine("Your deducted score is: " + deducted);
+            }
+            Console.ReadKey();
+        }
+    }
+}
